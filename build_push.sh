191a48bc18522095970aa075b389dc19713cb6d3
k8s-class <44496537+k8s-class@users.noreply.github.com>
2018-11-08 02:25:19 -0600

    Delete build_push.sh

--- build_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-             git clone https://github.com/buildmystartupio/basicnodeapp.git
-             cd basicnodeapp
-             docker build -t yourdockerrepo/basicnodeapp .
-             docker push yourdockerrepo/basicnodeapp
-             kubectl run basicnodeapp --image=yourdockerrepo/basicnodeapp --port=3000
-             kubectl expose deployment basicnodeapp --type=NodePort
-             kubectl get service
